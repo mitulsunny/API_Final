e0dc6d2a9865fa37b1a892fbb700d65d8dd11d86
mitulsunny <41156630+mitulsunny@users.noreply.github.com>
2020-10-22 19:26:55 -0400

    All four http request is added here

--- RestAssuredFramework/src/main/java/com/osa/base/JsonPayload.java
+++ RestAssuredFramework/src/main/java/com/osa/base/JsonPayload.java
@@ -5,6 +5,17 @@ import java.util.HashMap;
 import org.json.simple.JSONObject;
 
 public class JsonPayload {
+	public String email="george.bluth@reqres.in";
+	public  String patchBody() {
+		
+		
+		String body="{\"email\":\""+email+"\",\"first_name\": \"George\",\"last_name\": \"Bluth\",\"avatar\": \"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg\"}";
+		return body;
+	}
+	public static void main(String[] args) {
+		JsonPayload js=new JsonPayload();
+		System.out.println(js.patchBody());
+	}
 	
 	/**
 	 * This method will that HashMap as a parameter 
