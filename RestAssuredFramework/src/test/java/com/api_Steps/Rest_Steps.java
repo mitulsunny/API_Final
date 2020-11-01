d706e41caa279c8b1feb033a0f58d672f63f2cc0
shuvo <theking420620@gmail.com>
2020-11-01 01:47:03 -0400

    adding delete

--- RestAssuredFramework/src/test/java/com/api_Steps/Rest_Steps.java
+++ RestAssuredFramework/src/test/java/com/api_Steps/Rest_Steps.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.json.simple.JSONObject;
+
 import com.base.callAPI;
 import com.osa.base.JsonPayload;
 import com.osa.utilities.Rest_Utility;
@@ -12,10 +15,12 @@ import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.response.ValidatableResponse;
 
 public class Rest_Steps {
-	Response r;
-	public static String l;
+	public static Response r;
+	public static JSONObject l;
 
 	@Given("^I want to create a new user with the following information$")
 	public void i_want_to_create_a_new_user_with_the_following_information() throws Throwable {
@@ -35,19 +40,26 @@ public class Rest_Steps {
 
 		l = JsonPayload.payload(store);
 		r = callAPI.executeRquest(arg1, arg2);
+		
 
 
+		
 	}
-	@Then("^I verify first name$")
-	public void i_verify_first_name(DataTable data) throws Throwable {
+	
+	@Then("^it should create a new user$")
+	public void it_should_create_a_new_user() throws Throwable {
+		System.out.println("User created Sucessfully");
+	}
+	
+
+		
+	@Then("^I verify all the VAL$")
+	public void i_verify_all_the_VAL(DataTable data) throws Throwable {
 		Map<String,Map<String, String>> info =Rest_Utility.mangeInputData(data);
-	     
-		Map<String,String> keyy= info.get("VAL");
-		for (Map.Entry<String, String> entry : keyy.entrySet()) {
-			System.out.println(entry.getKey()+ " "+ entry.getValue());
-      
-		}
+		
+		Rest_Utility.printVerify(info,r);
 	}
+	
 
 	@Then("^Later I validate the HTTP Status code$")
 	public void later_I_validate_the_HTTP_Status_code() throws Throwable {
@@ -57,10 +69,20 @@ public class Rest_Steps {
 			System.out.println("An error HTTP code " + r.statusCode() + " occured");
 		}
 	}
-
-	@Then("^it should create a new user$")
-	public void it_should_create_a_new_user() throws Throwable {
-		System.out.println("User created Sucessfully");
+	
+	
+	@Then("^I want the  new user to be delted  through \"([^\"]*)\" HTTP Method using the endpoint \"([^\"]*)\"$")
+	public void i_want_the_new_user_to_be_delted_through_HTTP_Method_using_the_endpoint(String arg1, String arg2) throws Throwable {
+		
+		   
+		  ResponseBody o = r.body(); 
+		  o =callAPI.executeRquest(arg1,arg2);
+		  System.out.println("it's going to delete now");
+		  o.prettyPrint();
+		  
+            	
 	}
+
+	
 
 }
